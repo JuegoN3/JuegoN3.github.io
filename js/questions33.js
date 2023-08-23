ce56f439427693fb5f114addede3c23a63684dbd
JuegoN3 <143033661+JuegoN3@users.noreply.github.com>
2023-08-23 14:15:34 -0600

    Creando mi primer sitio web

---
+++ js/questions33.js
@@ -0,0 +1,358 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Como explicamos la sexta estrofa de nuestro Himno Nacional?",
+    answer: "Explicamos cuando Honduras tomo el ejemplo de Francia.",
+    options: [
+      "Explicamos la coquista.",
+      "Explicamos cuando Honduras tomo el ejemplo de Francia.",
+      "Explicamos la revolucion.",
+      "Explicamos la esclavitud."
+    ]
+  },
+    {
+    numb: 2,
+    question: "Cuando llegaron los pliegos que contenian la proclama de independecia a nuestro pais?",
+    answer: "28 de Septiembre de 1821.",
+    options: [
+      "23 de abril 1990.",
+      "31 de diciembre 1502.",
+      "1 de enero de 2000.",
+      "28 de Septiembre de 1821."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que alusion nos da la septima estrofa de nuestro Himno?",
+    answer: "Defender Nuestra Patria.",
+    options: [
+      "Defender Nuestra Patria.",
+      "Defender el bosque.",
+      "Defender los mares.",
+      "Defender los cielos."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que sentimiento resalta la septima estrofa del Himno?",
+    answer: "Amor, Patriotismo y Respeto.",
+    options: [
+      "Lealtal, Humildad y Vida.",
+      "Unidad, Igual y Amor.",
+      "Humildad, Honestidad y Amor.",
+      "Amor, Patriotismo y Respeto."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Que es la Bandera?",
+    answer: "Es el simbolo que representa la Nacion.",
+    options: [
+      "Es el simbolo que representa la Nacion.",
+      "Es un procer.",
+      "Es un simbolo menor.",
+      "Es un animal."
+    ]
+  },
+  {
+    numb: 6,
+    question: "Que simboliza la Bandera?",
+    answer: "Es el manto sagrado de la Patria.",
+    options: [
+      "Es la representacion de la belleza.",
+      "Es amor, Respeto y Paz.",
+      "Es el escudo Sagrado.",
+      "Es el manto sagrado de la Patria."
+    ]
+  },
+  {
+    numb: 7,
+    question: "En que fecha se celebra el dia de la Bandera Nacional?",
+    answer: "1 de Septiembre.",
+    options: [
+      "24 de Diciembre.",
+      "1 de Septiembre.",
+      "14 de Febrero.",
+      "31 de Diciembre."
+    ]
+  },
+    {
+    numb: 8,
+    question: "En que fecha y con que decreto se aprobo la Bandera Nacional?",
+    answer: "Decreto No. 7 del 16 de febrero de 1866.",
+    options: [
+      "Decreto No. 3 del 16 de febrero de 1866.",
+      "Decreto No. 1 del 20 de marzo de 1900.",
+      "1 de enero de 2000 decreto No. 9.",
+      "Decreto No. 7 del 16 de febrero de 1866."
+    ]
+  },
+    {
+    numb: 9,
+    question: "Que simbolizan las franjas azules de la Bandera Nacional?",
+    answer: "Los 2 mares y el cielo.",
+    options: [
+      "Los 2 mares y el cielo.",
+      "Las minas y nubes.",
+      "Las playas y mares.",
+      "Los bosques y rios."
+    ]
+  },
+    {
+    numb: 10,
+    question: "Que simbolizan las franja blanca de la Bandera Nacional?",
+    answer: "La paz y la pureza.",
+    options: [
+      "El respeto y pureza.",
+      "Unidad, Igual y Amor.",
+      "La union y amistad.",
+      "La paz y la pureza."
+    ]
+  },
+    {
+    numb: 11,
+    question: "Que simbolizan las 5 estrellas de la Bandera?",
+    answer: "Los 5 paises Centroamericanos.",
+    options: [
+      "Los 5 paises Centroamericanos.",
+      "La estrellas del cielo.",
+      "Los proceres nacionales.",
+      "El respeto y pureza."
+    ]
+  },
+  {
+    numb: 12,
+    question: "Que bandera habia durante el periodo colonial en Honduras?",
+    answer: "El Pabellon Real.",
+    options: [
+      "La granadera.",
+      "El pino.",
+      "La paz.",
+      "El Pabellon Real."
+    ]
+  },
+  {
+    numb: 13,
+    question: "Que bandera utilizo Honduras despues de independizarse de España?",
+    answer: "La Bandera de la union Centroamericana.",
+    options: [
+      "La bandera de España.",
+      "La bandera de la Paz.",
+      "La bandera de la Iglesia.",
+      "La Bandera de la union Centroamericana."
+    ]
+  },
+  {
+    numb: 14,
+    question: "Como debe doblarse la Bandera Nacional?",
+    answer: "En forma de triangulo.",
+    options: [
+      "En forma de circulo.",
+      "En forma de triangulo.",
+      "En forma de rombo.",
+      "En forma de cuadrado."
+    ]
+  },
+    {
+    numb: 15,
+    question: "Como se define el Escudo?",
+    answer: "Se define como el campo.",
+    options: [
+      "Se define como el mar.",
+      "Se define como las montañas.",
+      "Se define como la naturaleza.",
+      "Se define como el campo."
+    ]
+  },
+    {
+    numb: 16,
+    question: "Cual es el decreto mediante el cual se establecio el Escudo Nacional?",
+    answer: "Por medio del decreto No. 16 del 10 de Enero de 1935.",
+    options: [
+      "Por medio del decreto No. 16 del 10 de Enero de 1935.",
+      "Por medio del decreto No. 9 del 1 de Enero de 1935.",
+      "Por medio del decreto No. 10 del 11 de Marzo de 1935.",
+      "Por medio del decreto No. 12 del 10 de Febrero de 1935."
+    ]
+  },
+    {
+    numb: 17,
+    question: "Que significa la cordillera de Montañas?",
+    answer: "Que tiene un gran territorio.",
+    options: [
+      "Que tiene grandes rios.",
+      "Los pinos.",
+      "Que tiene Oceanos.",
+      "Que tiene un gran territorio."
+    ]
+  },
+    {
+    numb: 18,
+    question: "Que significan las bocaminas?",
+    answer: "La riquesa.",
+    options: [
+      "La riqueza.",
+      "Los Recursos Naturales.",
+      "La pobreza.",
+      "Los mares."
+    ]
+  },
+  {
+    numb: 19,
+    question: "Que significan los pinos que aparecen a la izquierda de cordillera de Montañas?",
+    answer: "El Arbol Naciona.",
+    options: [
+      "Es la representacion de la belleza.",
+      "Los Proceres.",
+      "El Ave Nacional.",
+      "El Arbol Naciona."
+    ]
+  },
+  {
+    numb: 20,
+    question: "Que significan las 5 herramientas que aparecen distribuidas en la cordillera de Montañas?",
+    answer: "El trabajo.",
+    options: [
+      "La esperanza.",
+      "El trabajo.",
+      "La unidad.",
+      "La pobreza."
+    ]
+  },
+    {
+    numb: 21,
+    question: "Que simboliza el ovalo?",
+    answer: "La Independencia.",
+    options: [
+      "La Guerra.",
+      "La Historia.",
+      "La Esclavitud.",
+      "La Independencia."
+    ]
+  },
+    {
+    numb: 22,
+    question: "Que significan el mar dentro del ovalo?",
+    answer: "La ruta que siguio Cristobal Colon.",
+    options: [
+      "La ruta que siguio Cristobal Colon.",
+      "Los Recursos Naturales.",
+      "Las playas y mares.",
+      "Los bosques y rios."
+    ]
+  },
+    {
+    numb: 23,
+    question: "Que representa el cielo que aparece dentro del ovalo del Escudo Nacional?",
+    answer: "Justicia, Verdad, Lealtad y Caridad.",
+    options: [
+      "El respeto y pureza.",
+      "Unidad, Igual y Amor.",
+      "La humildad y Humanidad.",
+      "Justicia, Verdad, Lealtad y Caridad."
+    ]
+  },
+    {
+    numb: 24,
+    question: "Que significa el triangulo equilatero que aparece dentro del ovalo?",
+    answer: "Igualdad ante la Ley.",
+    options: [
+      "Igualdad ante la Ley.",
+      "La estrellas del cielo.",
+      "Amistad y Amigos.",
+      "El respeto y pureza."
+    ]
+  },
+  {
+    numb: 25,
+    question: "Que representa el Volcan?",
+    answer: "La defensa de Nuestro Territorio.",
+    options: [
+      "La humildad y Humanidad.",
+      "Honestidad y Respeto.",
+      "La protecion de el bosque.",
+      "La defensa de Nuestro Territorio."
+    ]
+  },
+  {
+    numb: 26,
+    question: "Que significan los 2 castillos?",
+    answer: "El Valor y el Coraje.",
+    options: [
+      "Amor y Honestidad.",
+      "Los Recursos Naturales.",
+      "La Union Y Respeto.",
+      "El Valor y el Coraje."
+    ]
+  },
+  {
+    numb: 27,
+    question: "Que simboliza el Arcoiris?",
+    answer: "La Paz, Union y Armonia.",
+    options: [
+      "Amor y Honestidad.",
+      "La Paz, Union y Armonia.",
+      "Los Recursos Naturales.",
+      "La Union Y Respeto."
+    ]
+  },
+    {
+    numb: 28,
+    question: "Que simboliza el sol que aparece atras del Volcan?",
+    answer: "Libertad y Amor.",
+    options: [
+      "La Sociedad y Humanidad.",
+      "Igualda y Respeto.",
+      "Amistad y Amigos.",
+      "Libertad y Amor."
+    ]
+  },
+    {
+    numb: 29,
+    question: "Que representa la albaja llena de Flechas?",
+    answer: "Las armas con que se defendio la Patria.",
+    options: [
+      "Las armas con que se defendio la Patria.",
+      "Fuerza y Corje.",
+      "Inteligenci y Corja.",
+      "Defender los cielos."
+    ]
+  },
+    {
+    numb: 30,
+    question: "Que simbolizan los cuernos de abundancia que estan unidos por un lazo y que cuelgan de la aljaba de Flechas?",
+    answer: "La Riqueza Natural.",
+    options: [
+      "La Riqueza Natural.",
+      "Unidad, Igual y Amor.",
+      "Humildad, Honestidad y Amor.",
+      "La Riqueza Natural."
+    ]
+  },
+    {
+    numb: 31,
+    question: "Como esta compuesta la Bandera Nacional de Honduras?",
+    answer: "Por 2 franjas azules y 1 blanca en Medio.",
+    options: [
+      "Por 2 franjas azules y 1 blanca en Medio.",
+      "Por 3 frajas de color Azul.",
+      "Por 2 frajas azules y 1 Negra.",
+      "Por 2 franjas negras y 1 azul en Medio."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
